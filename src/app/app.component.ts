29d6ab5acd1ad43652fd4d85b99d4655c37b8c5e
agusteen <agustin.php@gmail.com>
2018-07-18 16:47:35 -0300

    form validations, reset, repeat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,63 +14,103 @@ export class AppComponent {
 
   barValue: number = 0;
   loading: Boolean = false;
+  flagSimulado = false;
+  flagSimuladoCompleto = false;
 
-  costo_acumuladoR1: number = 0;
-  costo_acumuladoR2: number = 0;
+  costo_acumulado: number = 0;
+
+  costo_acumuladoR1p: number = 0;
+  costo_acumuladoR2p: number = 0;
+
+  costo_acumuladoR1np: number = 0;
+  costo_acumuladoR2np: number = 0;
 
   simulacionesR1: Array<any>;
   simulacionesR2: Array<any>;
+
+  msg: string = 'Para cantidades mayor a 10 mil la performance degrada';
+  cancelar: boolean = false;
 
   constructor() {
 
   }
 
-  prepararSim(){
-    this.simulacionesR1 = new Array();
-    this.simulacionesR2 = new Array();
-    this.loading = true;
-    this.start();
-    this.showTable = true;
-    
+  prepararSim() {
+    if (this.cant_simulaciones > 0 && this.cant_simulaciones < 10000) {
+      this.simulacionesR1 = new Array();
+      this.simulacionesR2 = new Array();
+      this.loading = true;
+      this.start();
+      this.showTable = true;
+    } else {
+      this.msg = 'Debe ser un numero mayor a 0'
+    }
+
   }
 
   async start() {
     let update = Math.round(this.cant_simulaciones / 10);
 
-    
+
     for (let i = 1; i <= this.cant_simulaciones; i++) {
       let ruta: Recorrido[] = [];
       ruta1_rojo.forEach((value, index) => {
-        ruta.push(this.simular(i, value, index));
+        ruta.push(this.simular(i, value, index)[0]);
       });
-      if(i % update == 0){                
-        this.barValue = (i/this.cant_simulaciones)/2;
+      if (i % update == 0) {
+        if (this.cancelar) {
+          this.cancelar = false;
+          return;
+        }
+        this.barValue = (i / this.cant_simulaciones) / 2;
         await this.wait(0.01);
-      }      
-      this.simulacionesR1.push(ruta);  
+      }
+      this.simulacionesR1.push(ruta);
     }
+
+    this.guardarCosto(1);
 
     for (let i = 1; i <= this.cant_simulaciones; i++) {
       let ruta: Recorrido[] = [];
       ruta2_azul.forEach((value, index) => {
-        ruta.push(this.simular(i, value, index));
+        ruta.push(this.simular(i, value, index)[0]);
+
       });
-      if(i % update == 0){                
-        this.barValue = 0.5 + (i/this.cant_simulaciones)/2;
+      if (i % update == 0) {
+        if (this.cancelar) {
+          this.cancelar = false;
+          return;
+        }
+        this.barValue = 0.5 + (i / this.cant_simulaciones) / 2;
         await this.wait(0.01);
-      }      
-      this.simulacionesR2.push(ruta);  
-    }
-
-
-
-
-
+      }
+      this.simulacionesR2.push(ruta);
+    };
+    this.guardarCosto(2);
     this.loading = false;
+    this.flagSimulado = true;
   }
 
-  wait(ms: number)  {
-    return new Promise((resolve)=> {
+  guardarCosto(ruta) {
+    if (ruta == 1) {
+      if (this.paga) {
+        this.costo_acumuladoR1p = this.costo_acumulado;
+      } else {
+        this.costo_acumuladoR1np = this.costo_acumulado;
+      }
+      this.costo_acumulado = 0;
+    } else {
+      if (this.paga) {
+        this.costo_acumuladoR2p = this.costo_acumulado;
+      } else {
+        this.costo_acumuladoR2np = this.costo_acumulado;
+      }
+      this.costo_acumulado = 0;
+    }
+  }
+
+  wait(ms: number) {
+    return new Promise((resolve) => {
       setTimeout(resolve, ms);
     });
   }
@@ -79,7 +119,7 @@ export class AppComponent {
     return Math.floor(Math.random() * 100);
   }
 
-  simular(num: Number, ruta, semaforo): Recorrido {
+  simular(num: Number, ruta, semaforo): [Recorrido, Number] {
 
     var simulacionRuta: Recorrido;
     simulacionRuta = new Recorrido();
@@ -105,7 +145,7 @@ export class AppComponent {
           simulacionRuta.limpia = true;
           if (this.paga) {
             simulacionRuta.costo = 0.5;
-            this.costo_acumuladoR1 += 0.5;            
+            this.costo_acumulado += 0.5;
           } else {
             let rnd_rompe = this.getRandomNumber();
 
@@ -114,14 +154,39 @@ export class AppComponent {
             if (rnd_rompe < ruta.lim_daño) {
               simulacionRuta.rompe = true;
               simulacionRuta.costo = 500;
-              this.costo_acumuladoR1 += 500;
+              this.costo_acumulado += 500;
             }
           }
         }
       }
     }
 
-    return simulacionRuta;
+    return [simulacionRuta, this.costo_acumulado];
+  }
+
+  repetir() {
+    this.flagSimuladoCompleto = true;
+    this.paga = !this.paga;
+    this.loading = true;
+    this.start();
+    this.showTable = true;
+  }
+
+  reset() {
+    this.flagSimulado = false;
+    this.costo_acumulado = 0;
+    this.costo_acumuladoR1p = 0;
+    this.costo_acumuladoR2p = 0;
+    this.costo_acumuladoR1np = 0;
+    this.costo_acumuladoR2np = 0;
+    this.showTable = false;
+    this.flagSimuladoCompleto = false;
+  }
+
+  cancel() {
+    this.cancelar = true;
+    this.loading = false;
+    this.reset();
   }
 
 }
